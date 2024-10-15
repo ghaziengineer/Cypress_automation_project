459ca51bacbffd6eb6c397f1eb477fb748ed3be6
ghaziabdallah01 <ghaziiabdalah@gmail.com>
2024-10-15 14:46:51 +0100

    avoid timout error

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+chromeWebSecurity: false,
+defaultCommandTimeout: 15000,
+
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
