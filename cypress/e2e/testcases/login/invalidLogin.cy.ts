1726055c1654655d0eb95c18518bb988ca6f0b97
ghaziabdallah01 <ghaziiabdalah@gmail.com>
2024-10-15 22:20:37 +0100

    enhance invalid login code quality

--- cypress/e2e/testcases/login/invalidLogin.cy.ts
+++ cypress/e2e/testcases/login/invalidLogin.cy.ts
@@ -1,9 +1,9 @@
 describe('invalid Login testcase', () => {
   it('should not log in successfully with invalid credentials', () => {
     cy.visit('/');
-    cy.get('#user-name').type('ghazi');
-    cy.get('#password').type('ghazi_engineer');
-    cy.get('#login-button').click();
+   cy.get('[data-test="username"]')..type('ghazi');
+   cy.get('[data-test="password"]').type('ghazi_engineer');
+   cy.get('[data-test="login-button"]').click();
 
     // Verify unsuccessful login
     cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
